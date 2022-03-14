dd839e195fcc3236e0d78d6466d5e3cdce5f55bd
zthzhang111 <v_zthzhang@tencent.com>
2022-03-14 13:27:34 +0800

    Revert "修改参数isTotal判断方式"
    This reverts commit d5eb7b9b4871ce0d8bf50e7cdd44284e06e5796c.

--- plugins/jenkins_plugin/src/main/java/io/jenkins/plugins/TCABuilder.java
+++ plugins/jenkins_plugin/src/main/java/io/jenkins/plugins/TCABuilder.java
@@ -110,8 +110,6 @@ public class TCABuilder extends Builder implements SimpleBuildStep {
         }
         if(total) {
             isTotal = " --total";
-        }else{
-            isTotal = " ";
         }
         String clientPath = codeAnalysisPath + "client";
         StartClient.startClient(osName,
