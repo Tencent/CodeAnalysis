676d9aa1852109ae94b545ce8591aac1e8974e2a
Faberiii <qawb@foxmail.com>
2022-06-08 17:25:24 +0800

    添加删除代码库功能

--- web/packages/tca-analysis/src/modules/repos/repo-list/tabs/overview.tsx
+++ web/packages/tca-analysis/src/modules/repos/repo-list/tabs/overview.tsx
@@ -20,13 +20,15 @@ interface IProps {
   orgSid: string;
   teamName: string;
   repoId: number;
+  deletable: boolean;
+  onDelete: () => void;
 }
 
 const layout = {
   labelCol: { span: 4 },
 };
 
-const Overview = ({ curRepo, orgSid, teamName, repoId }: IProps) => {
+const Overview = ({ curRepo, orgSid, teamName, repoId, deletable, onDelete }: IProps) => {
   const [form] = Form.useForm();
   const [edit, setEdit] = useState(false);
   const dispatch = useDispatchStore();
@@ -50,63 +52,66 @@ const Overview = ({ curRepo, orgSid, teamName, repoId }: IProps) => {
   };
 
   return (
-        <Form
-            {...layout}
-            initialValues={curRepo || {}}
-            form={form}
-            style={{ width: '530px', marginTop: '30px' }}
-            onFinish={values => onFinish(values.name)}
-        >
-            <Form.Item label={t('仓库地址')} name="scm_url">
-                <span style={{ wordBreak: 'break-all' }}>{curRepo.scm_url}</span>
-            </Form.Item>
-            <Form.Item
-                label={t('仓库别名')}
-                name="name"
-                rules={edit ? [{ required: true, message: t('仓库别名为必填项') }] : undefined}
-            >
-                {edit ? <Input width={400} /> : <span>{curRepo.name}</span>}
-            </Form.Item>
-            <Form.Item label={t('创建人')} name="creator">
-                <>
-                    <Avatar
-                        size="small"
-                        src={creatorInfo.avatar_url || getUserImgUrl(creatorInfo.nickname)}
-                        alt={creatorInfo.nickname}
-                        icon={<UserIcon />}
-                    />
-                    <span className=" inline-block vertical-middle ml-sm">
-                        {creatorInfo.nickname}
-                    </span>
-                </>
-            </Form.Item>
+    <Form
+      {...layout}
+      initialValues={curRepo || {}}
+      form={form}
+      style={{ width: '530px', marginTop: '30px' }}
+      onFinish={values => onFinish(values.name)}
+    >
+      <Form.Item label={t('仓库地址')} name="scm_url">
+        <span style={{ wordBreak: 'break-all' }}>{curRepo.scm_url}</span>
+      </Form.Item>
+      <Form.Item
+        label={t('仓库别名')}
+        name="name"
+        rules={edit ? [{ required: true, message: t('仓库别名为必填项') }] : undefined}
+      >
+        {edit ? <Input width={400} /> : <span>{curRepo.name}</span>}
+      </Form.Item>
+      <Form.Item label={t('创建人')} name="creator">
+        <>
+          <Avatar
+            size="small"
+            src={creatorInfo.avatar_url || getUserImgUrl(creatorInfo.nickname)}
+            alt={creatorInfo.nickname}
+            icon={<UserIcon />}
+          />
+          <span className=" inline-block vertical-middle ml-sm">
+            {creatorInfo.nickname}
+          </span>
+        </>
+      </Form.Item>
 
-            <Form.Item label={t('创建时间')} name="created_time">
-                <span>{formatDateTime(curRepo.created_time)}</span>
-            </Form.Item>
+      <Form.Item label={t('创建时间')} name="created_time">
+        <span>{formatDateTime(curRepo.created_time)}</span>
+      </Form.Item>
 
-            <div style={{ marginTop: '30px' }}>
-                {edit ? (
-                    <>
-                        <Button type="primary" htmlType="submit" key="submit">
-                            {t('确定')}
-                        </Button>
-                        <Button className=" ml-12" htmlType="button" onClick={onReset}>
-                            {t('取消')}
-                        </Button>
-                    </>
-                ) : (
-                    <Button
-                        key="edit"
-                        htmlType="button"
-                        type="primary"
-                        onClick={() => setEdit(true)}
-                    >
-                        {t('编辑')}
-                    </Button>
-                )}
-            </div>
-        </Form>
+      <div style={{ marginTop: '30px' }}>
+        {edit ? (
+          <>
+            <Button type="primary" htmlType="submit" key="submit">
+              {t('确定')}
+            </Button>
+            <Button className=" ml-12" htmlType="button" onClick={onReset}>
+              {t('取消')}
+            </Button>
+          </>
+        ) : (
+          <Button
+            key="edit"
+            htmlType="button"
+            type="primary"
+            onClick={() => setEdit(true)}
+          >
+            {t('编辑')}
+          </Button>
+        )}
+        {deletable && <Button className=" ml-12" htmlType="button" onClick={onDelete} danger type='primary'>
+          {t('删除代码库')}
+        </Button>}
+      </div>
+    </Form>
   );
 };
 export default Overview;
