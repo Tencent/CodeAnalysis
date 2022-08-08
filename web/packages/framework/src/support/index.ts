fdeaeb411ea4e43718f56e138fe48d220686882a
nickctang <nickctang@tencent.com>
2022-08-08 16:17:08 +0800

    :art: 调整基座代码结构

--- web/packages/framework/src/support/index.ts
+++ web/packages/framework/src/support/index.ts
@@ -1,9 +1,3 @@
-// Copyright (c) 2021-2022 THL A29 Limited
-//
-// This source code file is made available under MIT License
-// See LICENSE for details
-// ==============================================================================
-
 import './unsupported-browser';
 import './version';
 import './expose';
