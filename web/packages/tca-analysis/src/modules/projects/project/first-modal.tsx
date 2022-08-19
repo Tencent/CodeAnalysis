49fe4fab37ac31c93f526861b7bb10b91b522d37
Faberiii <qawb@foxmail.com>
2022-08-19 15:25:09 +0800

    fix: 选择分支

--- web/packages/tca-analysis/src/modules/projects/project/first-modal.tsx
+++ web/packages/tca-analysis/src/modules/projects/project/first-modal.tsx
@@ -57,8 +57,6 @@ const FirstModal = (props: FirstModalProps) => {
 
   const onFinish = (data: any) => {
     const { funcList = [] } = data;
-    // 开源版需要隐藏tag，默认赋予tag Codedog_Linux
-    const tag = tags.filter(item => item.public && item.name === 'Codedog_Linux').pop() || tags.pop();
     data = data.type === 'create' ? {
       branch: data.branch,
       scan_scheme: {
@@ -72,7 +70,6 @@ const FirstModal = (props: FirstModalProps) => {
         envs: null,
         pre_cmd: null,
         build_flag: false,
-        tag: tag.name || 'Codedog_Linux',
       },
     } : {
       branch: data.branch,
