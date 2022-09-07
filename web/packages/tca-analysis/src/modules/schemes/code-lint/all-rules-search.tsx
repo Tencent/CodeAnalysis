2649047308904e543565be0717f2f310deea0ccc
freyaLo <freya_lo@163.com>
2022-09-07 19:39:03 +0800

    :art: issue 列表展示 msg 信息

--- web/packages/tca-analysis/src/modules/schemes/code-lint/all-rules-search.tsx
+++ web/packages/tca-analysis/src/modules/schemes/code-lint/all-rules-search.tsx
@@ -5,26 +5,27 @@
 // ==============================================================================
 
 import React, { useEffect } from 'react';
-import { toNumber, isString, isEmpty, isArray, cloneDeep } from 'lodash';
-import { Input, Form, Button } from 'coding-oa-uikit';
+import { toNumber, isString, isEmpty, isArray, cloneDeep, find } from 'lodash';
+import { Input, Form, Button, Select } from 'coding-oa-uikit';
 
 import SelectBorderless from '@src/components/select-borderless';
 import Filter from '@src/components/filter';
 import { SEVERITY, CATEGORY } from '../constants';
 
-const numberParams = ['checkpackage', 'severity', 'category', 'checktool'];
+const numberParams = ['checkpackage', 'severity', 'category'];
 const arrayParams = ['language'];
 
 interface SearchProps {
   filters: any;
   searchParams: any;
   loading: boolean;
+  addToolsRules: (toolId: number) => void;  // 一键添加工具所有规则
   callback: (params: any) => void;
 }
 
 const Search = (props: SearchProps) => {
   const [form] = Form.useForm();
-  const { searchParams, loading, filters, callback } = props;
+  const { searchParams, loading, filters, addToolsRules, callback } = props;
   const { allPkgs = [], languages = [], checkTools = [] }: any = filters;
 
   const initialValues = cloneDeep(searchParams);
@@ -34,6 +35,10 @@ const Search = (props: SearchProps) => {
       initialValues[key] = value
         .split(',')
         .map((item: string) => toNumber(item));
+    }
+
+    if (initialValues.checktool) {
+      initialValues.checktool = toNumber(initialValues.checktool);
     }
 
     if (arrayParams.includes(key) && isString(value)) {
@@ -104,19 +109,6 @@ const Search = (props: SearchProps) => {
           }
         />
       </Filter.Item>
-      <Filter.Item label="所属工具" name="checktool">
-        <SelectBorderless
-          multiple
-          allowClear
-          placeholder="全部"
-          data={checkTools?.map((item: any) => ({
-            value: item.id,
-            text: item.display_name,
-          }))}
-          onChange={(value: any) => value && onChange('checktool', value.join(','))
-          }
-        />
-      </Filter.Item>
       <Filter.Item label="适用语言" name="language">
         <SelectBorderless
           multiple
@@ -130,20 +122,57 @@ const Search = (props: SearchProps) => {
           }
         />
       </Filter.Item>
+      <Filter.Item label="所属工具" name="checktool">
+        <Select
+          showSearch
+          allowClear
+          size='middle'
+          placeholder='全部工具'
+          style={{ minWidth: '150px' }}
+          optionFilterProp='children'
+          dropdownMatchSelectWidth={false}
+          onChange={(value: any) => onChange('checktool', value)}
+        >
+          {
+            checkTools.map((item: any) => (
+              <Select.Option key={item.id} value={item.id}>{item.display_name}</Select.Option>
+            ))
+          }
+        </Select>
+        {/* <SelectBorderless
+          allowClear
+          placeholder="全部"
+          data={checkTools?.map((item: any) => ({
+            value: item.id,
+            text: item.display_name,
+          }))}
+          onChange={(value: any) => value && onChange('checktool', value.join(','))
+          }
+        /> */}
+      </Filter.Item>
+
       <Filter.Item label="" name="real_name">
         <Input.Search
           size="middle"
           placeholder="规则名称"
+          style={{ minWidth: '150px' }}
           onSearch={value => onChange('real_name', value)}
         />
       </Filter.Item>
       {Object.keys(searchParams).some((key: string) => (isArray(searchParams[key])
         ? !isEmpty(searchParams[key])
         : searchParams[key])) && (
-        <Button type="link" onClick={onClear} style={{ height: '36px' }}>
+        <Button type="link" onClick={onClear} style={{ height: '36px', marginRight: 10 }}>
           清空过滤
         </Button>
       )}
+      {
+        find(checkTools, { id: initialValues.checktool }) && (
+          <Button type="link" style={{ height: '36px' }} onClick={() => addToolsRules(initialValues.checktool)}>
+            一键添加 {find(checkTools, { id: initialValues.checktool })?.display_name} 工具的所有规则
+          </Button>
+        )
+      }
     </Filter>
   );
 };
