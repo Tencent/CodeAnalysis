5b9d01e1645a0c666fafe1186994d3921527405d
cyw3 <2927096163@qq.com>
2024-03-01 20:14:48 +0800

    :art: update Copyright
    Signed-off-by: cyw3 <2927096163@qq.com>

--- client/util/cleaner.py
+++ client/util/cleaner.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-# Copyright (c) 2022 THL A29 Limited
+# Copyright (c) 2021-2024 THL A29 Limited
 #
 # This source code file is made available under MIT License
 # See LICENSE for details
